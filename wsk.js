d3dbe770a7ca8ee0c9e6d4d739c8ea1f72270a29
Wk0p <57257114+Wk0p@users.noreply.github.com>
2020-12-09 20:18:32 +0000

    videos embeded
    3 * 3 videos embedded as examples

--- wsk.js
+++ wsk.js
@@ -1,3 +1,5 @@
+// Scroll
+
 function scroll(e){
 ​
     var href = $(this).attr('href');
@@ -13,3 +15,24 @@ function scroll(e){
 };
 ​
 $('a[href^="#"]').click(scroll);
+
+
+// Instagram
+
+(function($){
+		$(window).on('load', function(){
+			$.instagramFeed({
+				'username': 'callofduty',
+				'container': "#instagram-feed-demo",
+				'display_profile': true,
+				'display_biography': true,
+				'display_gallery': true,
+				'get_raw_json': false,
+				'callback': null,
+				'styling': true,
+				'items': 36,
+				'items_per_row': 6,
+				'margin': 0.3
+			});
+		});
+	})
